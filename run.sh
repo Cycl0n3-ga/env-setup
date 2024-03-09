045de39b6051680c9d321356095385acd30f633c
cyclonegao <jdio1881@gmail.com>
2024-03-09 23:53:15 +0800

    change docker install method

--- run.sh
+++ run.sh
@@ -10,21 +10,8 @@ rm ~/.vimrc
 cp vimrc ~/.vimrc
 
 #=============================install docker 
-# Add Docker's official GPG key:
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-#Post-installation set-up without sudo
-groupadd docker
-usermod -aG docker $USER
-newgrp docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 #=============================install docker 
 
 # Install Zsh and set it as default shell
@@ -45,3 +32,7 @@ zsh
 p10k configure
 
 
+#Post-installation set-up without sudo
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
